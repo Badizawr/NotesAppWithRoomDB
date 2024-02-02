2bc5e767b6869e7342ddda6fa50fa839cf041486
Badizawr <vicodin-13@ya.ru>
2024-02-02 18:30:16 +0500

    create database

---
+++ app/src/main/java/ru/vorobev/notesappwithroomdb/data/NoteDao.kt
@@ -0,0 +1,22 @@
+package ru.vorobev.notesappwithroomdb.data
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Upsert
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface NoteDao {
+    @Upsert
+    suspend fun upsertNote(note: Note)
+
+    @Delete
+    suspend fun deleteNote(note: Note)
+    @Query("SELECT * FROM note ORDER BY dateAdded")
+    fun getNotesOrderByDateAdded(): Flow<List<Note>>
+
+    @Query("SELECT * FROM note ORDER BY title ASC")
+    fun getNotesOrderByTitle(): Flow<List<Note>>
+}
